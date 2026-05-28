f3d0f40297ebf6ba5f96238112701c325ede3bde
Kartikay Awasthi <ikartikay.awasthi@gmail.com>
2026-05-28 17:20:07 +0530

    modal update bcz submit article button is not showing

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -3,19 +3,43 @@ export default function Modal({
   children,
   onClose,
 }) {
+
   if (!open) return null;
 
   return (
     <div
-      className="fixed inset-0 bg-black/40 flex justify-center items-center z-50"
+      className="
+        fixed
+        inset-0
+        bg-black/40
+        flex
+        justify-center
+        items-center
+        z-50
+        p-6
+      "
       onClick={onClose}
     >
+
       <div
-        className="bg-white rounded-xl p-6 w-full max-w-xl"
-        onClick={(e) => e.stopPropagation()}
+        className="
+          bg-white
+          rounded-3xl
+          w-full
+          max-w-4xl
+          max-h-[90vh]
+          overflow-y-auto
+          shadow-2xl
+        "
+        onClick={(e) =>
+          e.stopPropagation()
+        }
       >
+
         {children}
+
       </div>
+
     </div>
   );
 }
